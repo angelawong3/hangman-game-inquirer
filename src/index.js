da066c20c4ea13526a723b0995869e5fc0c587a2
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-13 23:06:22 +0100

    fs and path

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 // inquirer package
 const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
 
 // import questions
 const { gameSettingQuestions } = require("./questions");
+
+const getWordListByCategory = (category) => {
+  // buile the file path
+  const fileName = `${category}.json`;
+  const filePath = path.join(__dirname, "data", fileName);
+
+  // read from file using file path
+  const data = fs.readFileSync(filePath, "utf-8");
+
+  // return data from file
+  return data;
+};
 
 const init = async () => {
   // prompt the game setting question and store answers
@@ -10,8 +24,11 @@ const init = async () => {
 
   //   log the answers
   console.log(answers);
+
+  // ask the catergory and console log the data from the chosen category
+  const wordList = getWordListByCategory(answers.category);
+
+  console.log(wordList);
 };
 
 init();
-
-// ask the catergory and console log the data from the chosen category
